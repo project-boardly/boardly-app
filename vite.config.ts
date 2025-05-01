deda979c8f8dfbe5a170da0f9bacc750b2272c62
kriss1897 <kriss1897@users.noreply.github.com>
2025-05-01 19:07:21 +0530

    Add TipTap Editor
    - disable groopProof function

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 // import { VitePWA } from "vite-plugin-pwa";
 import { comlink } from "vite-plugin-comlink";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { fileURLToPath, URL } from "node:url";
+import { readFileSync } from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,18 +18,13 @@ export default defineConfig({
       
   //   },
   // },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./@", import.meta.url))
+    }
+  },
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   injectRegister: 'auto',
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,jpeg,jpg}']
-    //   }
-    // }),
     comlink(),
     nodePolyfills()
   ],
